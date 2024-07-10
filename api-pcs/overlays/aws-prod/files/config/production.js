3c3fb34220c003bd0408cd9116f0a1327ab994c9
Celina Chouali <celinachouali47@gmail.com>
2024-07-10 14:26:04 +0200

    Configuration kubernates

---
+++ api-pcs/overlays/aws-prod/files/config/production.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const domain = 'bleme.fr';
+
+module.exports = {
+  domain,
+  server: {
+    host: '0.0.0.0',
+    port: 80
+  },
+  payment: {
+    host:     'http://api-payment.bleme.svc.cluster.local',
+    port: 80
+  },
+  database: {
+    client: 'pg',
+    version: '15',
+    connection: {
+      host:     'postgres.bleme.svc.cluster.local',
+      database: 'postgres',
+      user:     'postgres',
+      password: 'postgres',
+      port:     5432
+    }
+  },
+  strategies: {
+    account: {
+      name: 'account',
+      cookie: {
+        name: 'auth',
+        ttl: 1000 * 60 * 60 * 24 * 7 * 2,
+        isSecure: true,
+        isHttpOnly: true,
+        encoding: 'iron'
+      }
+    },
+    adherent: {
+      name: 'adherent',
+      cookie: {
+        name: 'adherent',
+        ttl: 1000 * 60 * 60 * 24 * 7 * 2,
+        isSecure: true,
+        isHttpOnly: true,
+        encoding: 'iron'
+      }
+    }
+  },
+  rabbitmq: {
+    connection: {
+      protocol: 'amqp',
+      hostname: 'rabbitmq-queue.bleme.svc.cluster.local',
+      port:     5672,
+      username: 'admin',
+      password: 'admin',
+      vhost:    'bleme',
+    }
+  }
+}
